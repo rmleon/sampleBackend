30ff918ae8500980e85bb20b009cb6790c24de72
Ricardo Leon <ricardo@quanticmind.com>
2016-08-09 00:24:50 -0700

    adding cors

--- app/Filters.scala
+++ app/Filters.scala
@@ -2,24 +2,10 @@
   * @author Ricardo Leon
   */
 
-//import javax.inject.Inject
-//
-//import play.api.http.HttpFilters
-//import play.api.mvc.EssentialFilter
-//import play.filters.hosts.AllowedHostsFilter
-//import play.filters.cors.CORSFilter
-//
-//class Filters @Inject()(allowedHostsFilter: AllowedHostsFilter)
-//  extends HttpFilters(allowedHostsFilter) {
-//  def filters {
-//    CORSFilter()
-//  }
-//}
-
 import javax.inject.Inject
 
 import play.api.http.DefaultHttpFilters
 import play.filters.cors.CORSFilter
 
-class Filters @Inject() (corsFilter: CORSFilter)
+class Filters @Inject()(corsFilter: CORSFilter)
   extends DefaultHttpFilters(corsFilter)
